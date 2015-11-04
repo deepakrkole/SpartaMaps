cbe25f44d5753cc55d3421afd40b007eb996e53e
Jatin <jatin@vinays-macbook-pro.local>
2015-11-03 16:05:20 -0800

    Initial Commit

---
+++ SpartaMaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpartaMaps
+//
+//  Created by Jatin on 11/3/15.
+//  Copyright © 2015 Jatin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
